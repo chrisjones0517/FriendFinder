2cace0439cfaa7588c5230ab30ac1f4d5b7c7487
Chris Jones <chrisjones0517@gmail.com>
2018-04-22 14:37:28 -0500

    logo test

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+const public = require('path').join(__dirname,'/app/public');
+
+app.use(express.static(public));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
